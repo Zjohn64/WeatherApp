444ab848ba7aa4c4ea16a55338ae08eccf13fa53
Zachary Johnson <bballzach64@gmail.com>
2018-08-18 12:06:06 -0400

    Set up API callbacks and Handled errors

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-const request = require('request');
+const yargs = require('yargs');
 
-request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1531%20N%20Pearl%20Street%20Jacksonville',
-  json: true
-  
-}, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+const geocode = require('./geocode/geocode')
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather data',
+      string: true
+    }
+})
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  if (errorMessage) {
+    console.log(errorMessage);
+  } else {
+    console.log(JSON.stringify(results, undefined, 2));
+  }
 });
+
+
 
